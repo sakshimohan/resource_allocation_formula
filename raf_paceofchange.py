cfd07cf318d6660984e593029c60000c5136a709
sakshimohan <47076591+sakshimohan@users.noreply.github.com>
2019-02-21 21:30:55 +0200

    Update raf_paceofchange.py

--- raf_paceofchange.py
+++ raf_paceofchange.py
@@ -20,9 +20,9 @@ B[0] =  1000000 # This needs to be edited with the actual national resource enve
 g = 0.10 # annual growth rate of total budget
 
 
-for i in range(M-1):
+for year in range(M-1):
 
-    B[i+1]=B[i]*(1+g)
+    B[year+1]=B[year]*(1+g)
 
 
 # Define distribution array
@@ -37,9 +37,9 @@ D[0] = D_init
 A = [[None]*N]*M
 
 
-# Calculate allocation in starting year (Year 0)
+# Calculate allocation in first year
 
-A[0] = [D[0][j]*B[0] for j in range(N)]
+A[0] = [D[0][district]*B[0] for district in range(N)]
 
 # Temporary arrays for use during annual iterations:
 
@@ -56,34 +56,34 @@ actual_delta = [None] * N
 
 ############################################
 
-for i in range(M-1): # loop through years
+for year in range(M-1): # loop through years
 
     
 
-    previous_alloc = A[i]
+    previous_alloc = A[year]
 
     this_alloc = [None]*N
 
 
-    for j in range(N):
+    for district in range(N):
 
-        desired_budget[j] = D_targ[j]*B[i+1]  # desired allocation in year i+1
+        desired_budget[district] = D_targ[district]*B[year+1]  # desired allocation in year i+1
 
-        desired_delta[j] = max(0, desired_budget[j] - A[i][j])
+        desired_delta[district] = max(0, desired_budget[district] - A[year][district])
 
-    for j in range(N):
+    for district in range(N):
 
-        total_delta = B[i+1] - B[i]
+        total_delta = B[year+1] - B[year]
 
-        actual_delta[j] = total_delta*desired_delta[j]/sum(desired_delta)
+        actual_delta[district] = total_delta*desired_delta[district]/sum(desired_delta)
 
-        this_alloc[j] = round(previous_alloc[j] + actual_delta[j],0)
+        this_alloc[district] = round(previous_alloc[district] + actual_delta[district],0)
 
         
 
-    A[i + 1] =  this_alloc
+    A[year + 1] =  this_alloc
 
-    D[i+1] = [round(A[i+1][j]/B[i+1],4) for j in range(N)]
+    D[year+1] = [round(A[year+1][district]/B[year+1],4) for district in range(N)]
 
 
 ############################################
@@ -96,17 +96,17 @@ for i in range(M-1): # loop through years
 
 print("Year by year distributions")
 
-for i in range(M):
+for year in range(M):
 
-    print("year", i, " :", D[i])
+    print("year", year, " :", D[year])
 
 
 
 print("Year by year allocations")
 
-for i in range(M):
+for year in range(M):
 
-    print("year", i, " :", A[i])
+    print("year", year, " :", A[year])
 
 
 
@@ -117,7 +117,7 @@ for i in range(M):
 
 ############################################
 
-R = [D_init[j]/D_targ[j] for j in range(N)]
+R = [D_init[district]/D_targ[district] for district  in range(N)]
 
 Rmax = max(R)
 
